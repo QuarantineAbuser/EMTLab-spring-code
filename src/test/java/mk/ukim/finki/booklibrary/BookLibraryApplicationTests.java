7f613d5c92123e75aef936950f2493aff6243a8f
QuarantineAbuser <stefanprilep7@gmail.com>
2022-04-12 14:15:33 +0200

    Initial commit

---
+++ src/test/java/mk/ukim/finki/booklibrary/BookLibraryApplicationTests.java
@@ -0,0 +1,13 @@
+package mk.ukim.finki.booklibrary;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookLibraryApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
