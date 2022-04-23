0a567661898cf4e7481c95cbc9c0371267e92964
QuarantineAbuser <stefanprilep7@gmail.com>
2022-04-23 15:28:17 +0200

    Cross Origin to Heroku

--- src/main/java/mk/ukim/finki/booklibrary/web/BookRestController.java
+++ src/main/java/mk/ukim/finki/booklibrary/web/BookRestController.java
@@ -10,7 +10,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
-@CrossOrigin(origins = "http://localhost:3000")
+@CrossOrigin(origins = "https://library-spring-195091.herokuapp.com")
 @RequestMapping("/api/books")
 public class BookRestController {
 
